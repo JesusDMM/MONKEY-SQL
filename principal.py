50b94ddf39618f7f57c022c7f7287030895a5ea2
Jesus Daniel <mendoza.medina.21057@itsmante.edu.mx>
2024-10-06 12:55:07 -0600

    implementacion de analizador lexico

--- principal.py
+++ principal.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from analizadores.analizador_lexico import *
 
 app = Flask(__name__, static_folder='static')
 
@@ -9,6 +10,25 @@ def principal():
 @app.route('/semantico')
 def semantico():
     return render_template('semantico.html') 
+
+@app.route('/api/v1/analizador_lexico', methods=['POST'])
+def crear_usuario():
+    data = request.get_json()
+    consulta = data['texto']
+    if consulta is None or consulta.strip() == "":
+        return jsonify({"Errore": 'Consulta vacía'}), 400
+    tokens, errores, tok = tokenizar(consulta)
+    
+    tokens_formateados = '\n'.join(
+        [', '.join(map(str, tokens[i:i+4])) for i in range(0, len(tokens), 4)]
+    )
+    
+    errores_formateados = '\n'.join(
+        [', '.join(errores[i:i+4]) for i in range(0, len(errores), 4)]
+    )
+    
+    return jsonify({'Tokens': tokens_formateados,
+                    'Error': errores_formateados}), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
