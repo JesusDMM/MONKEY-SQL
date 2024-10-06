0d4594d432c05c359dde9cea4e33188473c26d1a
Jesus Daniel <mendoza.medina.21057@itsmante.edu.mx>
2024-10-06 11:39:20 -0600

    incorporacion de flask con las plantillas hmtl

--- principal.py
+++ principal.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def principal():
-    return render_template('inicio.html')
+    return render_template('index.html')
+
+@app.route('/semantico')
+def semantico():
+    return render_template('semantico.html') 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
